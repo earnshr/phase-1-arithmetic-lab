b9699cf4d8fc42074e8cdb0c4ae44589704a374b
bexearn12 <earnshr@gmail.com>
2021-09-28 11:22:56 -0500

    commit tree ef6541750e47bffeca0d8bf0f5eb2042e0d8a442

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random());
+
+let num3 = 14;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 4, 20);
+
+
